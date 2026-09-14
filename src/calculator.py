ec6a96f48b5e55c1474522744064d4873d3b8228
stefandrg256 <dragos.stefan@hermits.org>
2026-09-14 18:30:02 +0000

    Implemented division feature for calculator

--- src/calculator.py
+++ src/calculator.py
@@ -14,11 +14,26 @@ def subtract(a, b):
     """Return b subtracted from a."""
     return a - b
 
+def divide(a, b):
+    """Return a divided by b"""
+
+    if (b==0):
+        return "Cannot divide by 0"
+    return round(float(a) / b, 4)
+
 
 def main():
     print("Engineering Calculator")
     print("5 + 3 =", add(5, 3))
     print("5 - 3 =", subtract(5, 3))
+
+    print("5 / 3 =", divide(5, 3))
+    print("-5 / 3 =", divide(-5, 3))
+    print("5 / 2 =", divide(5, 2))
+    print("15 / -3 =", divide(15, -3))
+    print("-5 / -3 =", divide(-5, -3))
+    print("5 / 0 =", divide(5, 0))
+    print("0 / 5 =", divide(0, 5))
 
 
 if __name__ == "__main__":
