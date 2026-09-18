0b689306c68b355c14a6d93e58f8ae1645fe19f6
stefandrg256 <dragos.stefan@hermits.org>
2026-09-18 18:18:06 +0000

    Combined all new features on main

--- src/calculator.py
+++ src/calculator.py
@@ -1,8 +1,22 @@
+import numbers
 """ATCS Unit 0 demonstration calculator.
 
 This program is intentionally simple so students can focus on
 professional software-engineering workflow rather than syntax.
 """
+a = ""
+b = ""
+while not isinstance(a, numbers.Number):
+    try:
+        a = float(input("Input a number for A: "))
+    except ValueError:
+        continue
+
+while not isinstance(b, numbers.Number):
+    try:
+        b = float(input("Input a number for B: "))
+    except ValueError:
+        continue
 
 
 def add(a, b):
@@ -14,6 +28,10 @@ def subtract(a, b):
     """Return b subtracted from a."""
     return a - b
 
+def multiply(a, b):
+    """Return the product of a and b."""
+    return a * b
+
 def divide(a, b):
     """Return a divided by b"""
 
@@ -24,17 +42,12 @@ def divide(a, b):
 
 def main():
     print("Engineering Calculator")
-    print("5 + 3 =", add(5, 3))
-    print("5 - 3 =", subtract(5, 3))
-
-    print("5 / 3 =", divide(5, 3))
-    print("-5 / 3 =", divide(-5, 3))
-    print("5 / 2 =", divide(5, 2))
-    print("15 / -3 =", divide(15, -3))
-    print("-5 / -3 =", divide(-5, -3))
-    print("5 / 0 =", divide(5, 0))
-    print("0 / 5 =", divide(0, 5))
+    print(a," + ",b," =", add(a, b))
+    print(a," - ",b," =", subtract(a, b))
+    print(a," * ",b," =", multiply(a, b))
+    print(a," / ",b," =", divide(a, b))
 
 
 if __name__ == "__main__":
     main()
+
